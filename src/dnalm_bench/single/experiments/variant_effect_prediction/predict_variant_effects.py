7d42e910b28e575e3b681cd04c39eab0d1e2b3e4
Arpita Singhal <arpitas@sh02-ln04.stanford.edu>
2024-03-04 18:03:19 -0800

    accounting for variants that don't have a matching allele in the reference at chr[pos-1]

--- src/dnalm_bench/single/experiments/variant_effect_prediction/predict_variant_effects.py
+++ src/dnalm_bench/single/experiments/variant_effect_prediction/predict_variant_effects.py
@@ -8,7 +8,7 @@ import os
 if __name__ == "__main__":
     model_name = "Mistral-DNA-v0.1"
     genome_fa = "/mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-    variants_bed = ".bed"
+    variants_bed = "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.CaQTLS.tsv"
     batch_size = 2048
     num_workers = 4
     seed = 0
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     out_dir = "/oak/stanford/groups/akundaje/projects/dnalm_benchmark/embeddings/variant_embeddings/Mistral-DNA-v0.1/"
     os.makedirs(out_dir, exist_ok=True)
-    out_path = os.path.join(out_dir, "variant_embeddings.h5")
+    out_path = os.path.join(out_dir, "Afr.CaQTLs.variant_embeddings.h5")
 
     dataset = VariantDataset(genome_fa, variants_bed, chroms, seed)
     extractor = MistralDNAVariantEmbeddingExtractor(model_name, batch_size, num_workers, device)
