35937fa5f73abe9fa4d0d56e75d7579595786089
Arpita Singhal <arpitas@sh03-ln02.stanford.edu>
2024-03-06 10:24:55 -0800

    revising variant embeddings

--- src/dnalm_bench/single/embeddings.py
+++ src/dnalm_bench/single/embeddings.py
@@ -105,3 +105,30 @@ class MistralDNAVariantEmbeddingExtractor(HFVariantEmbeddingExtractor):
         model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
         super().__init__(tokenizer, model, batch_size, num_workers, device)
 
+class GenaLMVariantEmbeddingExtractor(HFVariantEmbeddingExtractor):
+    def __init__(self, model_name, batch_size, num_workers, device):
+        model_name = f"AIRI-Institute/{model_name}"
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+        model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+        super().__init__(tokenizer, model, batch_size, num_workers, device)
+
+
+class HyenaDNAVariantEmbeddingExtractor(HFVariantEmbeddingExtractor):
+    def __init__(self, model_name, batch_size, num_workers, device):
+        model_name = f"LongSafari/{model_name}"
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, padding_side="right")
+        model =  AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+        super().__init__(tokenizer, model, batch_size, num_workers, device)
+
+    def tokenize(self, seqs):
+        seqs_str = onehot_to_chars(seqs)
+        encoded = self.tokenizer(seqs_str, return_tensors="pt", padding=True)
+        tokens = encoded["input_ids"]
+
+        return tokens, None
+
+    def detokenize(self, seqs, token_embeddings, _):
+        seq_embeddings = token_embeddings[:,:seqs.shape[1],:]
+
+        return seq_embeddings
+
