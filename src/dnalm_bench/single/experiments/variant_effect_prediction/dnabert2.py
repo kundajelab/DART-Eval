55657c1a954a548246b49b9e226c212d3392a020
ArpiS <arpitas@stanford.edu>
2024-03-07 18:58:30 -0800

    loading embedding variants and computing cosine distance

--- src/dnalm_bench/single/experiments/variant_effect_prediction/dnabert2.py
+++ src/dnalm_bench/single/experiments/variant_effect_prediction/dnabert2.py
@@ -1,6 +1,8 @@
 import argparse
 from tqdm.auto import tqdm
 from scipy import spatial
+
+from .embedding_variants import load_embeddings_and_compute_cosine_distance
 from ...embeddings import DNABERT2VariantEmbeddingExtractor
 from ...components import VariantDataset
 import os
@@ -23,4 +25,10 @@ if __name__ == "__main__":
     dataset = VariantDataset(genome_fa, variants_bed, chroms, seed)
     extractor = DNABERT2VariantEmbeddingExtractor(model_name, batch_size, num_workers, device)
     extractor.extract_embeddings(dataset, out_path, progress_bar=True)
+
+    cosine_distances  = load_embeddings_and_compute_cosine_distance(out_dir)
+
+    df = dataset.elements_df
+    df["cosine_distances"]=cosine_distances # make sure that the cosine distances correspond to the correct allele
+    df.write_csv(os.path.join(out_dir, "dnabert2.Afr.CaQTLs.cosine_distances.tsv"))
 
