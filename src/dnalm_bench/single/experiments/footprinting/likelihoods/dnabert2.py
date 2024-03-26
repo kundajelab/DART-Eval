f4be36ce17ec46c48e6be5cc60fbe94b4b0bccf3
Aman Patel <amanpatel101@gmail.com>
2024-03-26 19:14:44 +0000

    Changed footprinting experiments to the size-350 sequences

--- src/dnalm_bench/single/experiments/footprinting/likelihoods/dnabert2.py
+++ src/dnalm_bench/single/experiments/footprinting/likelihoods/dnabert2.py
@@ -8,14 +8,14 @@ from ....components import FootprintingDataset
 if __name__ == "__main__":
     model_name = "DNABERT-2-117M"
     #genome_fa = "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-    seq_table = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/regions/footprinting/footprint_dataset.txt"
+    seq_table = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/regions/footprinting/footprint_dataset_350.txt"
     # chroms = ["chr22"]
     batch_size = 64
     num_workers = 4
     seed = 0
     device = "cuda"
 
-    out_dir = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/likelihoods/footprinting/{model_name}/"
+    out_dir = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/likelihoods/footprinting_350/{model_name}/"
     os.makedirs(out_dir, exist_ok=True)
     out_path = os.path.join(out_dir, f"likelihoods.tsv")
 
