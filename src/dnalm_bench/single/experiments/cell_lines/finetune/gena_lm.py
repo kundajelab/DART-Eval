bb2a5b51d0c2673452d37320fd6f5e96ed029e4b
Austin Wang <austin.wang1357@gmail.com>
2024-04-03 16:30:53 -0700

    Tune fine-tuning

--- src/dnalm_bench/single/experiments/cell_lines/finetune/gena_lm.py
+++ src/dnalm_bench/single/experiments/cell_lines/finetune/gena_lm.py
@@ -70,14 +70,14 @@ if __name__ == "__main__":
     lora_alpha = 2 * lora_rank
     lora_dropout = 0.05
     
-    lr = 2e-3
+    lr = 1e-4
     wd = 0.01
     num_epochs = 2
 
     # cache_dir = os.environ["L_SCRATCH_JOB"]
     cache_dir = "/mnt/disks/ssd-0/dnalm_bench_cache"
 
-    # out_dir = f"/scratch/groups/akundaje/dnalm_benchmark/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v0" 
+    # out_dir = f"/scratch/groups/akundaje/dnalm_benchmark/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v1" 
     out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v1"    
     # out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/test"    
 
