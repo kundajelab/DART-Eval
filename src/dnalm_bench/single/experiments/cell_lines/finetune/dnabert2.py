5c2130806882ce65c493c2b39d4d41c09565592a
Austin Wang <austin.wang1357@gmail.com>
2024-04-05 16:18:01 +0000

    Finetuning params

--- src/dnalm_bench/single/experiments/cell_lines/finetune/dnabert2.py
+++ src/dnalm_bench/single/experiments/cell_lines/finetune/dnabert2.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     lora_alpha = 2 * lora_rank
     lora_dropout = 0.05
 
-    accumulate = 3
+    accumulate = 4
     
     lr = 1e-4
     wd = 0.01
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     cache_dir = "/mnt/disks/ssd-0/dnalm_bench_cache"
 
     # out_dir = f"/scratch/groups/akundaje/dnalm_benchmark/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v1" 
-    out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v5"    
+    out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v6"    
     # out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/test"    
 
     os.makedirs(out_dir, exist_ok=True)
