13ef2fcc7842d8bd62686ed148daa356b6b75479
Austin Wang <austin.wang1357@gmail.com>
2024-04-14 19:48:47 +0000

    Bump version

--- src/dnalm_bench/single/experiments/cell_lines/finetune/dnabert2.py
+++ src/dnalm_bench/single/experiments/cell_lines/finetune/dnabert2.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     cache_dir = "/mnt/disks/ssd-0/dnalm_bench_cache"
 
     # out_dir = f"/scratch/groups/akundaje/dnalm_benchmark/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v1" 
-    out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v7"    
+    out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/v8"    
     # out_dir = f"/home/atwang/dnalm_bench_data/predictors/cell_line_2114_ft/{model_name}/{cell_line}/test"    
 
     os.makedirs(out_dir, exist_ok=True)
