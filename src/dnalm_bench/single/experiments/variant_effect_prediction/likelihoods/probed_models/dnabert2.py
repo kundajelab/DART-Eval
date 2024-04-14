c46c8583f484834c7a028a965d3eb6534693e113
ArpiS <arpitas@stanford.edu>
2024-04-14 14:07:33 -0700

    Variant Effect Prediction using probed models

--- src/dnalm_bench/single/experiments/variant_effect_prediction/likelihoods/probed_models/dnabert2.py
+++ src/dnalm_bench/single/experiments/variant_effect_prediction/likelihoods/probed_models/dnabert2.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     model_name = "DNABERT-2-117M"
     
     # out_dir = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/variants/likelihoods/{model_name}/"
+    out_dir = f"/scratch/groups/akundaje/dnalm_benchmark/likelihoods/variants/probed_models/{model_name}/"
+    os.makedirs(out_dir, exist_ok=True)
 
     batch_size = 64
     num_workers = 4
@@ -18,23 +20,18 @@ if __name__ == "__main__":
     chroms=None
     
     variants_bed = sys.argv[1]
-    counts_tsv = sys.argv[2]
+    likelihood_tsv = sys.argv[2]
     genome_fa = sys.argv[3]
-    cell_line = sys.argv[4]
-
-    model_path = f"/scratch/groups/akundaje/dnalm_benchmark/predictors/cell_line_2114/{model_name}/{cell_line}/v3/checkpoint_149.pt"
-
-    out_dir = f"/scratch/groups/akundaje/dnalm_benchmark/likelihoods/variants/probed_models/{model_name}/{cell_line}/"
-    os.makedirs(out_dir, exist_ok=True)
+    model_path = sys.argv[4]
 
     # variants_beds = ["/oak/stanford/groups/akundaje/anusri/variant-benchmakring/gm12878.dsqtls.benchmarking.tsv",
     #                  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Eu.CaQTLS.tsv",
     #                  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.ASB.CaQTLS.tsv", 
     #                  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.CaQTLS.tsv"]
-    # likelihood_tsvs = ["gm12878.dsqtls.benchmarking.counts.tsv", 
-    #                    "Eu.CaQTLS.counts.tsv",
-    #                    "Afr.ASB.CaQTLS.counts.tsv", 
-    #                    "Afr.CaQTLS.counts.tsv"]
+    # likelihood_tsvs = ["gm12878.dsqtls.benchmarking_likelihoods.tsv", 
+    #                    "Eu.CaQTLS.likelihoods.tsv",
+    #                    "Afr.ASB.CaQTLS.likelihoods.tsv", 
+    #                    "Afr.CaQTLS.likelihoods.tsv"]
     # genome_fas = ["/oak/stanford/groups/akundaje/soumyak/refs/hg19/male.hg19.fa", 
     #               "/oak/stanford/groups/akundaje/soumyak/refs/hg19/male.hg19.fa",
     #               "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta",
@@ -44,8 +41,8 @@ if __name__ == "__main__":
     hidden_channels = 32
     kernel_size = 8
 
-    print(counts_tsv)
-    out_path = os.path.join(out_dir, f"{counts_tsv}")
+    print(likelihood_tsv)
+    out_path = os.path.join(out_dir, f"{likelihood_tsv}")
 
     dataset = VariantDataset(genome_fa, variants_bed, chroms, seed)
     model = CNNEmbeddingsPredictor(input_channels, hidden_channels, kernel_size)
