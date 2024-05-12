141c6ca1b5e85a0a61bcdc30d8bd3eb95dbac0eb
ArpiS <arpitas@stanford.edu>
2024-05-12 09:25:59 -0700

    variant filtering update

--- src/dnalm_bench/single/experiments/variant_effect_prediction/variant_tasks.py
+++ src/dnalm_bench/single/experiments/variant_effect_prediction/variant_tasks.py
@@ -138,6 +138,13 @@ def plot_auprc_auroc_cosine_distances():
         plt.legend()
         plt.show()
 
+def get_precision_recall_auc(ctrl_counts, sig_counts):
+    counts = np.concatenate([ctrl_counts, sig_counts])
+    labels = np.concatenate([np.zeros(len(ctrl_counts)), np.ones(len(sig_counts))])
+    auroc = roc_auc_score(labels, counts)
+    auprc = average_precision_score(labels, counts)
+    return auprc, auroc
+
 def sig_ctrl_variants_Eu_CaQTLs(likelihoods_data_path):
     eu_caqtls_data_path =  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Eu.CaQTLS.tsv"
     eu_caQTLs_df = pd.read_csv(eu_caqtls_data_path, sep="\t")
@@ -193,31 +200,65 @@ def sig_ctrl_variants_Eu_CaQTLs_probed_counts(counts_data_path, name="_scores"):
 
     return ctrl_likelihoods, sig_likelihoods, filtered_var_eu_caQTLs_df
 
-def sig_ctrl_variants_Afr_CaQTLs(likelihood_data_path):
-    afr_caqtls_data_path =  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.CaQTLS.tsv"
+def compute_change(filtered_variants_df):
+    filtered_variants_df["llm_logfc"] = filtered_variants_df["allele2_scores"]-filtered_variants_df["allele1_scores"]
+    return filtered_variants_df, np.abs(filtered_variants_df["llm_logfc"])
+
+def sig_ctrl_variants_Afr_CaQTLs(scores_data_path):
+    # afr_caqtls_data_path =  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.CaQTLS.tsv"
+    afr_caqtls_data_path =  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.CaQTLS.new_filter.tsv"
     afr_caQTLs_df = pd.read_csv(afr_caqtls_data_path, sep="\t")
-    likelihood = pd.read_csv(likelihood_data_path, sep="\t")
-    if afr_caQTLs_df.shape[0] == likelihood.shape[0]:
-        likelihoods_data = pd.concat([afr_caQTLs_df, likelihood], axis=1)
-        filtered_var_afr_caQTLs_df = likelihoods_data[(likelihoods_data["IsUsed"]==True)].copy(deep=True)
-        filtered_var_afr_caQTLs_df["llm_logfc"] = filtered_var_afr_caQTLs_df["allele2_scores"]-filtered_var_afr_caQTLs_df["allele1_scores"]
+    likelihoods = pd.read_csv(scores_data_path, sep="\t")
 
-        print("unique label values", np.unique(filtered_var_afr_caQTLs_df["label"]))
-        filtered_var_afrcaqtls_df_sig = filtered_var_afr_caQTLs_df[(filtered_var_afr_caQTLs_df["label"]==1) & (-np.log10(filtered_var_afr_caQTLs_df["pval"])>=3)]
-        filtered_var_afrcaqtls_df_ctrl = filtered_var_afr_caQTLs_df[(filtered_var_afr_caQTLs_df["label"]==0) & (-np.log10(filtered_var_afr_caQTLs_df["pval"])<3)]
+    if "allele1_scores" in likelihoods.columns:
+        columns = ["allele1_scores", "allele2_scores"]
+    else:
+        columns = ["cosine_distance"]
+
+    scores = likelihoods[["chr_hg38", "pos_hg38", "allele1", "allele2"] + columns]
+    if afr_caQTLs_df.shape[0] == scores.shape[0]:
+        likelihoods_data = pd.merge(afr_caQTLs_df, scores, on=["chr_hg38", "pos_hg38", "allele1", "allele2"])
+        filtered_var_afr_caQTLs_df = likelihoods_data[(likelihoods_data["IsUsed"]==True) & (likelihoods_data["in_peaks"]==True)].copy(deep=True)
+
+        print("unique label values", filtered_var_afr_caQTLs_df["label"].value_counts())
+
+        filtered_var_afrcaqtls_df_sig = filtered_var_afr_caQTLs_df[(filtered_var_afr_caQTLs_df["label"]==1)]
+        filtered_var_afrcaqtls_df_ctrl = filtered_var_afr_caQTLs_df[(filtered_var_afr_caQTLs_df["label"]==0)]
         
-        control_likelihoods = np.abs(filtered_var_afrcaqtls_df_ctrl["llm_logfc"])
-        sig_likelihoods = np.abs(filtered_var_afrcaqtls_df_sig["llm_logfc"]) 
+        # control_likelihoods = np.abs(filtered_var_afrcaqtls_df_ctrl["llm_logfc"])
+        # sig_likelihoods = np.abs(filtered_var_afrcaqtls_df_sig["llm_logfc"]) 
 
-        print(len(control_likelihoods), len(sig_likelihoods))
+        # print(len(control_likelihoods), len(sig_likelihoods))
 
-        counts_ctrl, bins_ctrl = np.histogram(control_likelihoods, bins=100)
+        # counts_ctrl, bins_ctrl = np.histogram(control_likelihoods, bins=100)
 
-        counts_sig, bins_sig = np.histogram(sig_likelihoods, bins=100)
+        # counts_sig, bins_sig = np.histogram(sig_likelihoods, bins=100)
 
-        U1, p = mannwhitneyu(counts_ctrl, counts_sig, alternative="greater")
+        # U1, p = mannwhitneyu(counts_ctrl, counts_sig, alternative="greater")
 
-        return control_likelihoods, sig_likelihoods, filtered_var_afr_caQTLs_df
+        return filtered_var_afrcaqtls_df_ctrl, filtered_var_afrcaqtls_df_sig
+    
+def variants_Yoruba_LCL_dsQTLs(scores_data_path):
+    yoruba_dsqtls_data_path =  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/gm12878.dsqtls.benchmarking.tsv"
+    yoruba_dsQTLs_df = pd.read_csv(yoruba_dsqtls_data_path, sep="\t")
+    likelihoods = pd.read_csv(scores_data_path, sep="\t")
+
+    if "allele1_scores" in likelihoods.columns:
+        columns = ["allele1_scores", "allele2_scores"]
+    else:
+        columns = ["cosine_distance"]
+
+    scores = likelihoods[["var.chrom", "var.pos", "var.allele1", "var.allele2"]+columns]
+    if yoruba_dsQTLs_df.shape[0] == scores.shape[0]:
+        likelihoods_data = pd.merge(yoruba_dsQTLs_df, scores, on=["var.chrom", "var.pos", "var.allele1", "var.allele2"])
+        filtered_var_yoruba_dsQTLs_df = likelihoods_data[likelihoods_data["var.isused"]==True].copy(deep=True)
+
+        print("unique label values", filtered_var_yoruba_dsQTLs_df["var.label"].value_counts())
+
+        filtered_var_yoruba_dsqtls_df_sig = filtered_var_yoruba_dsQTLs_df[(filtered_var_yoruba_dsQTLs_df["var.label"]==1)]
+        filtered_var_yoruba_dsqtls_df_ctrl = filtered_var_yoruba_dsQTLs_df[(filtered_var_yoruba_dsQTLs_df["var.label"]==-1)]
+        
+        return filtered_var_yoruba_dsqtls_df_ctrl, filtered_var_yoruba_dsqtls_df_sig
     
 def sig_ctrl_variants_Afr_CaQTLs_probed_counts(counts_data_path):
     
@@ -282,6 +323,8 @@ def beta_logfc(filtered_df, title, ylabel="LogFC Scores"):
     plt.grid()
     plt.show()
 
+    return pearson_corr, spearman_corr  
+
 def effect_size_logfc(filtered_df, title):
     x = filtered_df["meanLog2FC"]
     y = filtered_df["llm_logfc"]
@@ -298,3 +341,22 @@ def effect_size_logfc(filtered_df, title):
                 x=0.5, y=0.98, ha='center')
     plt.grid()
     plt.show()
+    return pearson_corr, spearman_corr  
+
+def est_size_logfc(filtered_df, title, ylabel="LogFC Scores"):
+    x = filtered_df["obs.estimate"]
+    y = filtered_df["llm_logfc"]
+    g = sns.jointplot(x=x, y=y, 
+                    kind="scatter")
+
+    pearson_corr, _ = pearsonr(x, y)
+    spearman_corr, _ = spearmanr(x, y)
+
+    plt.subplots_adjust(top=0.9) 
+    plt.xlabel("Significant Allele-Specific Binding LogFC")
+    plt.ylabel("LogFC Scores")
+    g.figure.suptitle(f'{title}\nPearson: {pearson_corr:.4f} --- Spearman: {spearman_corr:.4f}', 
+                x=0.5, y=0.98, ha='center')
+    plt.grid()
+    plt.show()
+    return pearson_corr, spearman_corr
