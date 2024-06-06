4635f30c8774eb75d0b196f055ed1896062c57ed
ArpiS <arpitas@stanford.edu>
2024-06-06 14:30:46 -0700

    peak_classification_extract_embeddings

--- src/dnalm_bench/single/experiments/peak_classification/extract_embeddings/gena_lm.py
+++ src/dnalm_bench/single/experiments/peak_classification/extract_embeddings/gena_lm.py
@@ -4,24 +4,20 @@ import sys
 from ....embeddings import GENALMEmbeddingExtractor
 from ....components import SimpleSequence
 
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
     model_name = "gena-lm-bert-large-t2t"
-    #genome_fa = "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-    genome_fa = "/mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-
-    elements_tsv = "/oak/stanford/groups/akundaje/projects/dnalm_benchmark/cell_line_data/peaks_by_cell_label_unique_dataloader_format.tsv"    
+    genome_fa = os.path.join(root_output_dir,"/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta")
+    elements_tsv = os.path.join(root_output_dir,"/task_3_cell-type-specific/processed_inputs/peaks_by_cell_label_unique_dataloader_format.tsv")
     chroms = None
     batch_size = 64
     num_workers = 4
     seed = 0
     device = "cuda"
 
-    out_path = f"/scratch/groups/akundaje/dnalm_benchmark/embeddings/peak_classification/{model_name}.h5"
-
-    print("Making dataset")
+    out_path = os.path.join(root_output_dir,f"/task_3_cell-type-specific/embeddings/{model_name}.h5")
+    
     dataset = SimpleSequence(genome_fa, elements_tsv, chroms, seed)
-    print("Creating extractor")
     extractor = GENALMEmbeddingExtractor(model_name, batch_size, num_workers, device)
-    print("Extracting")
     extractor.extract_embeddings(dataset, out_path, progress_bar=True)
--- src/dnalm_bench/single/experiments/peak_classification/extract_embeddings/sequence_baseline.py
+++ src/dnalm_bench/single/experiments/peak_classification/extract_embeddings/sequence_baseline.py
@@ -3,20 +3,20 @@ import os
 from ....embeddings import SequenceBaselineSimpleEmbeddingExtractor
 from ....components import SimpleSequence
 
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
     model_name = "sequence_baseline"
-    genome_fa = "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-    # genome_fa = "/mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
+    genome_fa = os.path.join(root_output_dir,"/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta")
 
-    elements_tsv = "/oak/stanford/groups/akundaje/projects/dnalm_benchmark/cell_line_data/peaks_by_cell_label_unique_dataloader_format.tsv"    
+    elements_tsv = os.path.join(root_output_dir,"/task_3_cell-type-specific/processed_inputs/peaks_by_cell_label_unique_dataloader_format.tsv")
     chroms = None
     batch_size = 64
     num_workers = 4
     seed = 0
     device = "cuda"
 
-    out_path = f"/scratch/groups/akundaje/dnalm_benchmark/embeddings/peak_classification_sequence_baseline/{model_name}.h5"
+    out_path = os.path.join(root_output_dir,f"/task_3_cell-type-specific/embeddings/{model_name}.h5")
 
     dataset = SimpleSequence(genome_fa, elements_tsv, chroms, seed)
     extractor = SequenceBaselineSimpleEmbeddingExtractor(batch_size, num_workers, device)
