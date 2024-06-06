43dbd6718fe554a44848e3db9517201cc2abe604
Aman Patel <amanpatel101@gmail.com>
2024-06-06 16:20:35 -0700

    Fixed paths for footprinting launcher code

--- src/dnalm_bench/single/experiments/footprinting/embeddings/hyenadna.py
+++ src/dnalm_bench/single/experiments/footprinting/embeddings/hyenadna.py
@@ -7,15 +7,13 @@ from ....components import FootprintingDataset
 
 if __name__ == "__main__":
     model_name = "hyenadna-large-1m-seqlen-hf"
-    #genome_fa = "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-    seq_table = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/regions/footprinting/footprint_dataset_350_v1.txt"
-    # chroms = ["chr22"]
+    seq_table = os.path.join(root_output_dir, f"task_2_footprinting/processed_data/footprint_dataset_350_v1.txt")
     batch_size = 64
     num_workers = 0
     seed = 0
     device = "cuda"
 
-    out_dir = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/embeddings/footprinting_350/{model_name}/"
+    out_dir = os.path.join(root_output_dir, f"task_2_footprinting/outputs/embeddings/footprinting_350/{model_name}/")
     os.makedirs(out_dir, exist_ok=True)
     out_path = os.path.join(out_dir, f"embeddings.h5")
 
--- src/dnalm_bench/single/experiments/footprinting/likelihoods/dnabert2.py
+++ src/dnalm_bench/single/experiments/footprinting/likelihoods/dnabert2.py
@@ -4,18 +4,18 @@ import sys
 from ....evaluators import DNABERT2Evaluator
 from ....components import FootprintingDataset
 
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
+
 
 if __name__ == "__main__":
     model_name = "DNABERT-2-117M"
-    #genome_fa = "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
-    seq_table = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/regions/footprinting/footprint_dataset_350_v1.txt"
-    # chroms = ["chr22"]
+    seq_table = os.path.join(root_output_dir, f"task_2_footprinting/processed_data/footprint_dataset_350_v1.txt")
     batch_size = 64
     num_workers = 0
     seed = 0
     device = "cuda"
 
-    out_dir = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/likelihoods/footprinting_350/{model_name}/"
+    out_dir = os.path.join(root_output_dir, f"task_2_footprinting/outputs/likelihoods/footprinting_350/{model_name}/")
     os.makedirs(out_dir, exist_ok=True)
     out_path = os.path.join(out_dir, f"likelihoods.tsv")
 
