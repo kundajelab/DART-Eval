2722ffc1d043241f6a507fb236290cc4228185af
Aman Patel <amanpatel101@gmail.com>
2024-06-06 10:46:37 -0700

    Example with root output dir

--- src/dnalm_bench/single/experiments/cell_lines/train/dnabert2.py
+++ src/dnalm_bench/single/experiments/cell_lines/train/dnabert2.py
@@ -3,19 +3,21 @@ import sys
 
 from torch.utils.data import DataLoader
 
-from ....training import AssayEmbeddingsDataset, InterleavedIterableDataset, CNNEmbeddingsPredictor, train_predictor
+# from ....training import AssayEmbeddingsDataset, InterleavedIterableDataset, CNNEmbeddingsPredictor, train_predictor
+root_output_dir = os.environ.get("OUT_DIR", "")
 
 
 if __name__ == "__main__":
     cell_line = sys.argv[1] #cell line name
     resume_checkpoint = int(sys.argv[2]) if len(sys.argv) > 2 else None
 
+    print(root_output_dir)
     model_name = "DNABERT-2-117M"
-    peaks_h5 = f"/scratch/groups/akundaje/dnalm_benchmark/embeddings/cell_line_2114/{model_name}/{cell_line}_peaks.h5"
-    nonpeaks_h5 = f"/scratch/groups/akundaje/dnalm_benchmark/embeddings/cell_line_2114/{model_name}/{cell_line}_nonpeaks.h5"
-    peaks_tsv = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/regions/cell_line_expanded_peaks/{cell_line}_peaks.bed"
-    nonpeaks_tsv = f"/oak/stanford/groups/akundaje/projects/dnalm_benchmark/regions/cell_line_expanded_peaks/{cell_line}_nonpeaks.bed"
-    assay_bw = f"/scratch/groups/akundaje/dnalm_benchmark/cell_line_data/{cell_line}_unstranded.bw"
+    peaks_h5 = f"{root_output_dir}/embeddings/cell_line_2114/{model_name}/{cell_line}_peaks.h5"
+    nonpeaks_h5 = f"{root_output_dir}/embeddings/cell_line_2114/{model_name}/{cell_line}_nonpeaks.h5"
+    peaks_tsv = f"{root_output_dir}/regions/cell_line_expanded_peaks/{cell_line}_peaks.bed"
+    nonpeaks_tsv = f"{root_output_dir}/regions/cell_line_expanded_peaks/{cell_line}_nonpeaks.bed"
+    assay_bw = f"{root_output_dir}/cell_line_data/{cell_line}_unstranded.bw"
 
     batch_size = 512
     num_workers = 0
@@ -67,16 +69,16 @@ if __name__ == "__main__":
     num_epochs = 150
 
     # out_dir = "/oak/stanford/groups/akundaje/projects/dnalm_benchmark/classifiers/ccre_test_regions_500_jitter_50/DNABERT-2-117M/v0"
-    out_dir = f"/scratch/groups/akundaje/chrombench/synapse/task_4_chromatin_activity/supervised_classifiers/probed/{model_name}/{cell_line}/v1"   
+    out_dir = f"{root_output_dir}/task_4_chromatin_activity/supervised_classifiers/probed/{model_name}/{cell_line}/v1"   
     os.makedirs(out_dir, exist_ok=True)
 
-    peaks_train_datset = AssayEmbeddingsDataset(peaks_h5, peaks_tsv, chroms_train, assay_bw, crop=crop)
-    nonpeaks_train_dataset = AssayEmbeddingsDataset(nonpeaks_h5, nonpeaks_tsv, chroms_train, assay_bw, crop=crop, downsample_ratio=10)
-    train_dataset = InterleavedIterableDataset([peaks_train_datset, nonpeaks_train_dataset])
+    # peaks_train_datset = AssayEmbeddingsDataset(peaks_h5, peaks_tsv, chroms_train, assay_bw, crop=crop)
+    # nonpeaks_train_dataset = AssayEmbeddingsDataset(nonpeaks_h5, nonpeaks_tsv, chroms_train, assay_bw, crop=crop, downsample_ratio=10)
+    # train_dataset = InterleavedIterableDataset([peaks_train_datset, nonpeaks_train_dataset])
 
-    peaks_val_dataset = AssayEmbeddingsDataset(peaks_h5, peaks_tsv, chroms_val, assay_bw, crop=crop)
-    nonpeaks_val_dataset = AssayEmbeddingsDataset(nonpeaks_h5, nonpeaks_tsv, chroms_val, assay_bw, crop=crop)
-    val_dataset = InterleavedIterableDataset([peaks_val_dataset, nonpeaks_val_dataset])
+    # peaks_val_dataset = AssayEmbeddingsDataset(peaks_h5, peaks_tsv, chroms_val, assay_bw, crop=crop)
+    # nonpeaks_val_dataset = AssayEmbeddingsDataset(nonpeaks_h5, nonpeaks_tsv, chroms_val, assay_bw, crop=crop)
+    # val_dataset = InterleavedIterableDataset([peaks_val_dataset, nonpeaks_val_dataset])
 
-    model = CNNEmbeddingsPredictor(input_channels, hidden_channels, kernel_size)
-    train_predictor(train_dataset, val_dataset, model, num_epochs, out_dir, batch_size, lr, num_workers, prefetch_factor, device, progress_bar=True, resume_from=resume_checkpoint)
+    # model = CNNEmbeddingsPredictor(input_channels, hidden_channels, kernel_size)
+    # train_predictor(train_dataset, val_dataset, model, num_epochs, out_dir, batch_size, lr, num_workers, prefetch_factor, device, progress_bar=True, resume_from=resume_checkpoint)
