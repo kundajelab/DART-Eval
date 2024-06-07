aed600623e3557ee0c03f8d207f3491e1fb0b482
ArpiS <arpitas@stanford.edu>
2024-06-06 17:03:05 -0700

    variant probed log counts

--- src/dnalm_bench/single/experiments/variant_effect_prediction/probed_log_counts/nucleotide_transformer.py
+++ src/dnalm_bench/single/experiments/variant_effect_prediction/probed_log_counts/nucleotide_transformer.py
@@ -8,12 +8,13 @@ import polars as pl
 import pandas as pd
 import numpy as np
 
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
     model_name = "nucleotide-transformer-v2-500m-multi-species"
 
     batch_size = 256
-    num_workers = 4
+    num_workers = 0
     seed = 0
     device = "cuda"
     chroms=None
@@ -21,30 +22,17 @@ if __name__ == "__main__":
     variants_bed = sys.argv[1]
     counts_tsv = sys.argv[2]
     genome_fa = sys.argv[3]
-    cell_line = sys.argv[4]
+    cell_line = "GM12878"
 
-    model_folder = f"/scratch/groups/akundaje/chrombench/synapse/task_4_chromatin_activity/supervised_classifiers/probed/{model_name}/{cell_line}/v1/"
+    model_folder = os.path.join(root_output_dir, f"/task_4_chromatin_activity/supervised_models/probed/{model_name}/{cell_line}/v1/")
     train_log = f"{model_folder}/train.log"
     df = pd.read_csv(train_log, sep="\t")
     checkpoint_num = int(df["epoch"][np.argmin(df["val_loss"])])
 
     model_path = f"{model_folder}/checkpoint_{checkpoint_num}.pt"
 
-    out_dir = f"/scratch/groups/akundaje/chrombench/synapse/task_5_variant_effect_prediction/outputs/{model_name}/"
+    out_dir = os.path.join(root_output_dir, f"/task_5_variant_effect_prediction/outputs/probed/{model_name}/")
     os.makedirs(out_dir, exist_ok=True)
-
-    # variants_beds = ["/oak/stanford/groups/akundaje/anusri/variant-benchmakring/gm12878.dsqtls.benchmarking.tsv",
-    #                  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Eu.CaQTLS.tsv",
-    #                  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.ASB.CaQTLS.tsv", 
-    #                  "/oak/stanford/groups/akundaje/anusri/variant-benchmakring/Afr.CaQTLS.tsv"]
-    # likelihood_tsvs = ["gm12878.dsqtls.benchmarking.counts.tsv", 
-    #                    "Eu.CaQTLS.counts.tsv",
-    #                    "Afr.ASB.CaQTLS.counts.tsv", 
-    #                    "Afr.CaQTLS.counts.tsv"]
-    # genome_fas = ["/oak/stanford/groups/akundaje/soumyak/refs/hg19/male.hg19.fa", 
-    #               "/oak/stanford/groups/akundaje/soumyak/refs/hg19/male.hg19.fa",
-    #               "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta",
-    #               "/oak/stanford/groups/akundaje/refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"]
     
     input_channels = 1024
     hidden_channels = 32
