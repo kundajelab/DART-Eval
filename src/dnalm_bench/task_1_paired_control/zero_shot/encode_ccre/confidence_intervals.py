790855413b2484712f22ae7937b48b7b39b6b794
Arpita Singhal <arpi.cutiepie1@gmail.com>
2024-08-16 06:33:18 +0000

    confidence intervals revision

---
+++ src/dnalm_bench/task_1_paired_control/zero_shot/encode_ccre/confidence_intervals.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from statsmodels.stats.proportion import proportion_confint
+import json
+import os
+
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
+
+hyena_table = os.path.join(root_output_dir,"task_1_ccre/zero_shot_outputs/likelihoods/hyenadna-large-1m-seqlen-hf/metrics.json")
+with open(hyena_table, "r") as f:
+    hyena_data = json.load(f)
+
+gena_table = os.path.join(root_output_dir,"task_1_ccre/zero_shot_outputs/likelihoods/gena-lm-bert-large-t2t/metrics.json")
+with open(gena_table, "r") as f:
+    gena_data = json.load(f)
+
+dnabert_table = os.path.join(root_output_dir,"task_1_ccre/zero_shot_outputs/likelihoods/DNABERT-2-117M/metrics.json")
+with open(dnabert_table, "r") as f:
+    dnabert_data = json.load(f) 
+
+nt_table = os.path.join(root_output_dir,"task_1_ccre/zero_shot_outputs/likelihoods/nucleotide-transformer-v2-500m-multi-species/metrics.json")
+with open(nt_table, "r") as f:
+    nt_data = json.load(f)
+
+def get_confidence_interval(data, num_ccres):
+    return proportion_confint(data["acc"]*num_ccres, num_ccres, method="normal")
+
+num_ccres=(2348855-1)
+print("Hyena DNA", get_confidence_interval(hyena_data, num_ccres))
+print("Gena LM", get_confidence_interval(gena_data, num_ccres))
+print("DNABert2", get_confidence_interval(dnabert_data, num_ccres))
+print("Nuc Transformer", get_confidence_interval(nt_data, num_ccres))
+
---
+++ src/dnalm_bench/task_2_5_single/experiments/task_3_peak_classification/confidence_intervals.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+from statsmodels.stats.proportion import proportion_confint
+import json
+import os
+
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
+
+hyena_table_probed = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/probed/hyenadna-large-1m-seqlen-hf/eval_test.json")
+with open(hyena_table_probed, "r") as f:
+    hyena_probed = json.load(f)
+
+hyena_table_finetuned = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/fine_tuned/hyenadna-large-1m-seqlen-hf/eval_test.json")
+with open(hyena_table_finetuned, "r") as f:
+    hyena_finetuned = json.load(f)
+
+gena_table_probed = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/probed/gena-lm-bert-large-t2t/eval_test.json")
+with open(gena_table_probed, "r") as f:
+    gena_probed = json.load(f)
+
+gena_table_finetuned = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/fine_tuned/gena-lm-bert-large-t2t/eval_test.json")
+with open(gena_table_finetuned, "r") as f:
+    gena_finetuned = json.load(f)
+
+dnabert_table_probed = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/probed/DNABERT-2-117M/eval_test.json")
+with open(dnabert_table_probed, "r") as f:
+    dnabert_probed = json.load(f) 
+
+dnabert_table_finetuned = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/fine_tuned/DNABERT-2-117M/eval_test.json")
+with open(dnabert_table_finetuned, "r") as f:
+    dnabert_finetuned = json.load(f)
+
+nt_table_probed = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/probed/nucleotide-transformer-v2-500m-multi-species/eval_test.json")
+with open(nt_table_probed, "r") as f:
+    nt_probed = json.load(f)
+
+nt_table_finetuned = os.path.join(root_output_dir,"task_3_cell-type-specific/supervised_model_outputs/fine_tuned/nucleotide-transformer-v2-500m-multi-species/eval_test.json")
+with open(nt_table_finetuned, "r") as f:
+    nt_finetuned = json.load(f)
+
+def get_confidence_interval(data, num_ccres):
+    return proportion_confint(data["test_acc"]*num_ccres, num_ccres, method="normal")
+
+num_ccres=(216747-1)
+print("PROBED")
+print("Hyena DNA", get_confidence_interval(hyena_probed, num_ccres))
+print("Gena LM", get_confidence_interval(gena_probed, num_ccres))
+print("DNABert2", get_confidence_interval(dnabert_probed, num_ccres))
+print("Nucleotide Transformer", get_confidence_interval(nt_probed, num_ccres))
+
+print("\nFINETUNED")
+print("Hyena DNA", get_confidence_interval(hyena_finetuned, num_ccres))
+print("Gena LM", get_confidence_interval(gena_finetuned, num_ccres))
+print("DNABert2", get_confidence_interval(dnabert_finetuned, num_ccres))
+print("Nucleotide Transformer", get_confidence_interval(nt_finetuned, num_ccres))
+
