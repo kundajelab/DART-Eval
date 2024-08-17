a8bbda8684fe4fab134bf5178d111d3974cf349e
Aman Patel <amanpatel101@gmail.com>
2024-08-17 20:21:20 +0000

    Fixed footprinting dataset file name

--- src/dnalm_bench/task_2_5_single/experiments/task_2_transcription_factor_binding/embeddings/hyenadna.py
+++ src/dnalm_bench/task_2_5_single/experiments/task_2_transcription_factor_binding/embeddings/hyenadna.py
@@ -8,7 +8,7 @@ root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
     model_name = "hyenadna-large-1m-seqlen-hf"
-    seq_table = os.path.join(root_output_dir,"task_2_footprinting/processed_data/footprint_dataset_350.txt")
+    seq_table = os.path.join(root_output_dir,"task_2_footprinting/processed_data/footprint_dataset_350_v1.txt")
     batch_size = 64
     num_workers = 0
     seed = 0
--- src/dnalm_bench/task_2_5_single/experiments/task_2_transcription_factor_binding/embeddings/nucleotide_transformer.py
+++ src/dnalm_bench/task_2_5_single/experiments/task_2_transcription_factor_binding/embeddings/nucleotide_transformer.py
@@ -8,7 +8,7 @@ root_output_dir = os.environ.get("DART_WORK_DIR", "")
 
 if __name__ == "__main__":
     model_name = "nucleotide-transformer-v2-500m-multi-species"
-    seq_table = os.path.join(root_output_dir,"task_2_footprinting/processed_data/footprint_dataset_350.txt")
+    seq_table = os.path.join(root_output_dir,"task_2_footprinting/processed_data/footprint_dataset_350_v1.txt")
     batch_size = 32
     num_workers = 0
     seed = 0
