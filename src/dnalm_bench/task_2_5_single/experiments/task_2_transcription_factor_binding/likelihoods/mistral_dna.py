b86bd14618ee3bfcf775bdc43696f516afe5dcbc
Aman Patel <amanpatel101@gmail.com>
2024-08-17 19:59:43 +0000

    Mistral DNA script for likelihood footprinting

---
+++ src/dnalm_bench/task_2_5_single/experiments/task_2_transcription_factor_binding/likelihoods/mistral_dna.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from ....evaluators import MistralEvaluator
+from ....components import FootprintingDataset
+
+root_output_dir = os.environ.get("DART_WORK_DIR", "")
+
+if __name__ == "__main__":
+    model_name = "Mistral-DNA-v1-1.6B-hg38"
+    seq_table = os.path.join(root_output_dir,"task_2_footprinting/processed_data/footprint_dataset_350.txt")
+    batch_size = 64
+    num_workers = 0
+    seed = 0
+    device = "cuda"
+
+    out_dir = os.path.join(root_output_dir,"task_2_footprinting/outputs/likelihoods/")
+    os.makedirs(out_dir, exist_ok=True)
+    out_path = os.path.join(root_output_dir,f"task_2_footprinting/outputs/likelihoods/{model_name}.tsv")
+
+    dataset = FootprintingDataset(seq_table, seed)
+    evaluator = MistralEvaluator(model_name, batch_size, num_workers, device)
+    evaluator.evaluate(dataset, out_path, progress_bar=True)
