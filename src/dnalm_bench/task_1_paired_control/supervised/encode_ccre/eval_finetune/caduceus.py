f0c56f9c5d0c5161fbd4fe103ca270477abbba15
Austin Wang <austin.wang1357@gmail.com>
2024-09-02 12:17:29 -0700

    Caduceus ccre evaluator

--- src/dnalm_bench/task_1_paired_control/supervised/encode_ccre/eval_finetune/caduceus.py
+++ src/dnalm_bench/task_1_paired_control/supervised/encode_ccre/eval_finetune/caduceus.py
@@ -1,21 +1,25 @@
 import os
 import sys
 
-from ....finetune import train_finetuned_classifier, CaduceusLoRAModel
+import torch
+import numpy as np
+import pandas as pd
+
+from ....finetune import evaluate_finetuned_classifier, CaduceusLoRAModel
 from ....components import PairedControlDataset
 
 work_dir = os.environ.get("DART_WORK_DIR", "")
 cache_dir = os.environ.get("DART_CACHE_DIR")
 
 if __name__ == "__main__":
-    resume_checkpoint = int(sys.argv[1]) if len(sys.argv) > 1 else None
+    eval_mode = sys.argv[1] if len(sys.argv) > 1 else "test"
 
     model_name = "caduceus-ps_seqlen-131k_d_model-256_n_layer-16"
 
     genome_fa = os.path.join(work_dir, "refs/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta")
     elements_tsv = os.path.join(work_dir, f"task_1_ccre/processed_inputs/ENCFF420VPZ_processed.tsv")
 
-    batch_size = 64
+    batch_size = 4096
     num_workers = 4
     prefetch_factor = 2
     seed = 0
@@ -54,25 +58,33 @@ if __name__ == "__main__":
         "chr22"
     ]
 
+    modes = {"train": chroms_train, "val": chroms_val, "test": chroms_test}
+
     emb_channels = 256
+
+    crop = 557
 
     lora_rank = 8
     lora_alpha = 2 * lora_rank
     lora_dropout = 0.05
 
-    accumulate = 2
-    
-    lr = 1e-4
-    wd = 0.01
-    num_epochs = 16
+    model_dir = os.path.join(work_dir, f"task_1_ccre/supervised_models/fine_tuned/{model_name}")
+    train_log = os.path.join(model_dir, "train.log")
+    df = pd.read_csv(train_log, sep="\t")
+    checkpoint_num = int(df["epoch"][np.argmin(df["val_loss"])])    
+    checkpoint_path = os.path.join(model_dir, f"checkpoint_{checkpoint_num}.pt")
 
-    out_dir = os.path.join(work_dir, f"task_1_ccre/supervised_models/fine_tuned/{model_name}")
+    out_dir = os.path.join(work_dir, f"task_1_ccre/supervised_model_outputs/fine_tuned/{model_name}")
+
     os.makedirs(out_dir, exist_ok=True)
+    out_path = os.path.join(out_dir, f"eval_{eval_mode}.json")
 
-    train_dataset = PairedControlDataset(genome_fa, elements_tsv, chroms_train, seed)
-    val_dataset = PairedControlDataset(genome_fa, elements_tsv, chroms_val, seed)
+    test_dataset = PairedControlDataset(genome_fa, elements_tsv, modes[eval_mode], seed)
 
     model = CaduceusLoRAModel(model_name, lora_rank, lora_alpha, lora_dropout, 2)
-    train_finetuned_classifier(train_dataset, val_dataset, model, num_epochs, out_dir, 
-                               batch_size, lr, wd, accumulate, num_workers, prefetch_factor, 
-                               device, progress_bar=True, resume_from=resume_checkpoint)
+    checkpoint_resume = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint_resume, strict=False)
+    metrics = evaluate_finetuned_classifier(test_dataset, model, out_path, batch_size, num_workers, prefetch_factor, device, progress_bar=True)
+    
+    for k, v in metrics.items():
+        print(f"{k}: {v}")
